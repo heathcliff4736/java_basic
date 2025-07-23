f16ba0d46e1160e1353b7f705c2193bd77581ebf
heathcliff4736 <lookout97@naver.com>
2025-07-23 19:39:18 +0900

    0723

---
+++ src/jungol/선택제어문_연습문제2.java
@@ -0,0 +1,23 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class 선택제어문_연습문제2 {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+
+
+        if (num1 > num2){
+
+            System.out.printf("입력받은 수 중 큰 수는 %s이고 작은 수는 %s입니다. ", num1 , num2);
+        } else {
+            
+            System.out.printf("입력받은 수 중 큰 수는 %s이고 작은 수는 %s입니다. ", num2 , num1);
+        }
+    }
+}
---
+++ src/jungol/선택제어문_자가진단1.java
@@ -0,0 +1,19 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class 선택제어문_자가진단1 {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+
+        System.out.printf("%s\n", num1);
+
+        if (num1 < 0){
+            System.out.println("minus");
+        } else {
+            System.out.println();
+        }
+    }
+}
