a47ec7e946c014ffb97dddf75d3a0b79d827d3f7
heathcliff4736 <lookout97@naver.com>
2025-07-29 17:51:57 +0900

    0729_09

--- src/notepadd0729.java
+++ src/notepadd0729.java
@@ -1,18 +1,19 @@
-package jungol.반복제어문3;
-
 import java.util.Scanner;
 
-public class 반복제어문3_형성평가10 {
+public class notepadd0729 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-//        int n = 4;
+//        int n = sc.nextInt();
+        int n = 3;
+        int x;
         int temp = 1;
         int j;
 
         for (int i = 0; i < n; i++) {
-            for (j = temp; j <= temp+n*2-1; j+=2) {
+            for (j = temp; j <= temp+n+1; j+=2) {
+//                if(j%10) System.out.print(j + " ");
+//                else System.out.print(j+" ");
                 System.out.print(j%10+" ");
             }
             temp = j;
