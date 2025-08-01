27a31f1c9dcd854416837d509014cfe3479d2ae2
heathcliff4736 <lookout97@naver.com>
2025-08-01 17:01:07 +0900

    0801_05

---
+++ src/jungol/문자열2/문자열2_연습문제1.java
@@ -0,0 +1,12 @@
+package jungol.문자열2;
+
+import java.util.Scanner;
+
+public class 문자열2_연습문제1 {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }
+}
