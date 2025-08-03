0de1cc472420069970ea795e10484ae94fefff5b
heathcliff4736 <78144253+heathcliff4736@users.noreply.github.com>
2025-08-03 19:53:57 +0900

    0803_06

--- src/jungol/배열2/배열2_형성평가10.java
+++ src/jungol/배열2/배열2_형성평가10.java
@@ -7,6 +7,18 @@ public class 배열2_형성평가10 {
 
     public static void main(String[] args) {
 
+        char array[][] = new char[3][5];
 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                array[i][j] = sc.next().charAt(0);
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(Character.toLowerCase(array[i][j]) + " ");
+            }
+            System.out.println();
+        }
     }
 }
