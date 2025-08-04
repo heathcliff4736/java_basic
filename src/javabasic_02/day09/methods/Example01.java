b1e96e4cac3a5ff643dde72e56c0e2e3d85e82c9
heathcliff4736 <lookout97@naver.com>
2025-08-04 10:57:05 +0900

    0804_01

---
+++ src/javabasic_02/day09/methods/Example01.java
@@ -0,0 +1,35 @@
+package javabasic_02.day09.methods;
+
+import java.util.Scanner;
+
+public class Example01 {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void method(){
+        System.out.println("method() 시작");
+//        System.out.println("static Method");
+//        System.out.println(" 5+6 = 11");
+        System.out.println("method() 끝");
+    }
+    
+    public static void main(String[] args) {
+//        method();   // 정적(static)메서드 호출
+//        method();
+//        method();
+//        method();
+//        method();
+
+        int counter = 0;
+        System.out.print("몇번을 수행할까요? ");
+        counter = in.nextInt();
+
+        for (int i = 0; i < counter; i++) {
+            System.out.println(i+1+"번째 호출");
+            method();
+        }
+
+        System.out.println("원하는 만큼 method()호출 후 여기로 돌아오지요");
+
+    }
+}
