6cbe075eca2c0660a4385333658cbb836678b540
heathcliff4736 <lookout97@naver.com>
2025-08-05 17:12:15 +0900

    0805_05

--- src/javabasic_02/day10/morningtest/BankApplication.java
+++ src/javabasic_02/day10/morningtest/BankApplication.java
@@ -55,11 +55,11 @@ public class BankApplication {
             if (accountNo.equals(arrayAccount[i].getAccountNo())) {
                 // 셋발란스 = 겟발란스 + 입력받은 발란스
                 arrayAccount[i].setBalance((balance - arrayAccount[i].getBalance()));
+                System.out.println("결과: 예금이 성공적으로 완료되었습니다.");
             } else {
                 System.out.println("해당하는 계좌가 존재하지 않습니다.");
             }
         }
-        System.out.println("결과: 예금이 성공적으로 완료되었습니다.");
     }
 
     // 4. 출금 기능           - private static void withdraw()
@@ -69,17 +69,23 @@ public class BankApplication {
         System.out.println("-------------------");
         System.out.print("계좌번호 : ");
         String accountNo = in.nextLine();
-        System.out.print("예금액 : ");
+        System.out.print("출금액 : ");
         int balance = Integer.parseInt(in.nextLine());
 
         for (int i = 0; i < accountCount; i++) {
             if (accountNo.equals(arrayAccount[i].getAccountNo())) {
-                arrayAccount[i].setBalance((balance - arrayAccount[i].getBalance()));
+                if ((arrayAccount[i].getBalance() - balance) >= 0) {
+                    arrayAccount[i].setBalance((arrayAccount[i].getBalance() - balance));
+                    System.out.println("결과: 출금이 성공적으로 완료되었습니다.");
+                //잔고보다 많은 금액을 출금하지 못하도록
+                } else {
+                    System.out.println("잔액이 부족합니다.");
+                }
             } else {
                 System.out.println("해당하는 계좌가 존재하지 않습니다.");
             }
         }
-        System.out.println("결과: 출금이 성공적으로 완료되었습니다.");
+
     }
 
     // 5. 프로그램 종료 기능   - private static void exit()
@@ -109,8 +115,6 @@ public class BankApplication {
                 case 5 -> exit();
                 default -> System.out.println("1~5의 숫자를 입력하세요6");
             }
-//            System.out.println("프로그램 종료");
         }
-
     }
 }
