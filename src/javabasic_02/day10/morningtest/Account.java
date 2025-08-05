d72559c393d1047914b7a43bb788176743d30991
heathcliff4736 <lookout97@naver.com>
2025-08-05 15:16:11 +0900

    0805_02

---
+++ src/javabasic_02/day10/morningtest/Account.java
@@ -0,0 +1,56 @@
+package javabasic_02.day10.morningtest;
+
+public class Account {
+
+    private int balance;    // 통장 (입출금 내역 및 잔고 확인)
+    private static final int MIN_BALANCE = 0;
+    private static final int MAX_BALANCE = 1000000;
+
+    private String accountNo;   // 계좌번호
+    private String owner;       // 소유자
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    Account(){}
+
+    // 은행에 가서 계좌개설 신청하면 개인정보를 은행에 등록하는 첫 과정
+    Account(String accountNo, String owner, int balance){
+        this.accountNo = accountNo;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+
+    public int getBalance() {
+
+        return this.balance;
+    }
+
+    public void setBalance(int balance) {
+
+        if (balance < MIN_BALANCE || balance > MAX_BALANCE) this.balance = getBalance();
+        else this.balance = balance;
+    }
+
+    public static void main(String[] args) {
+//        Account account = new Account();
+
+//        account.setBalance(10000);
+//        System.out.println(account.balance);
+//
+//        account.setBalance(-100);
+//        System.out.println(account.balance);
+//
+//        account.setBalance(3000);
+//        System.out.println(account.balance);
+
+    }
+
+
+}
