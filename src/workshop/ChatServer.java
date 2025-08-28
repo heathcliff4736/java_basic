38df7755c6356c6e90aba8041315b451e6465dd1
heathcliff4736 <78144253+heathcliff4736@users.noreply.github.com>
2025-08-29 02:40:33 +0900

    c

--- src/workshop/ChatServer.java
+++ src/workshop/ChatServer.java
@@ -14,13 +14,15 @@ public class ChatServer {
     private static final ExecutorService POOL = Executors.newCachedThreadPool();
     private static final AtomicInteger CLIENT_SEQ = new AtomicInteger(1);
     
-    // 클라이언트들 닉네임을 저장할 ArrayList 선언
+    // 현재 연결된 클라이언트들을 저장할 ArrayList 선언
     private static final ArrayList<PrintWriter> CLIENTS_LIST = new ArrayList<>();
+    // 닉네임 중복검사를 위해 닉네임을 저장할 HashSet 선언 -> 중복을 저장하지 못하게
+    private static final Set<String> NICKNAMES = new HashSet<>();
 
     public static void main(String[] args) {
         System.out.println("Server starting on port " + PORT);
 
-        // Ctrl+C 시 스레드 풀 정리 (서버를 Ctrl+C로 종료할 때 스레드 풀을 정리하도록 훅(hook) 등록)
+        // 서버를 Ctrl+C로 종료할 때 스레드 풀을 정리하도록 훅 등록
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("\n[Server] Shutting down...");
             POOL.shutdownNow();
@@ -37,9 +39,7 @@ public class ChatServer {
         } catch (IOException e) {
             System.err.println("[Server] Error: " + e.getMessage());
         }
-
     }
-
 
     private static class ClientHandler implements Runnable {
         private final Socket socket;
@@ -52,6 +52,7 @@ public class ChatServer {
 
         @Override
         public void run() {
+            String nickname = null;
             try (
                     BufferedReader in = new BufferedReader(
                             new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
@@ -59,19 +60,35 @@ public class ChatServer {
                             new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true)
             ) {
 
-                out.println("Welcome! You are Client#" + clientId + ". Type 'exit' to quit.");
+                out.println("Welcome! You are Client#" + clientId + ". Type [NICK nickname] to set nickname. Type 'exit' to quit.");
 
-                String nickname = null;
+
                 String nicknameInput = in.readLine();
                 if (nicknameInput != null && nicknameInput.startsWith("NICK ")) {
                     nickname = nicknameInput.substring(5).trim();
+                    // NICKNAMES 동시에 여러 스레드가 접근할 수 있으므로 synchronized로 락
+                    synchronized (NICKNAMES) {
+                        if (NICKNAMES.contains(nickname)) {
+                            out.println("ERR ... Nickname already used.");
+                            socket.close();
+                            return;
+                        }
+                        NICKNAMES.add(nickname);
+                    }
                     System.out.println("[Server] Client #" + clientId + ": " + nickname);
                 } else {
-                    out.println("잘못된 접근 방법입니다.");
+                    out.println("ERR ... 잘못된 접근 방법입니다.");
                     socket.close();
+                    return;
                 }
 
-                out.println(nickname + " joined");
+                // CLIENTS_LIST에 동시에 여러 스레드가 접근할 수 있으므로 synchronized로 락
+                synchronized (CLIENTS_LIST) {
+                    CLIENTS_LIST.add(out);  // 입장 시
+                }
+                broadcast("[System] " + nickname + " joined");
+
+
                 String line;
                 while ((line = in.readLine()) != null) {
                     System.out.println("[Server] From Client#" + nickname + ": " + line);
@@ -79,7 +96,7 @@ public class ChatServer {
                         out.println("Bye!");
                         break;
                     }
-                    out.println("Echo to #" + clientId + ":" + line);
+                    broadcast("[" + nickname + "] " + line);
                 }
             } catch (IOException e) {
                 System.err.println("[Server] Client#" + clientId + " I/O error: " + e.getMessage());
@@ -94,9 +111,9 @@ public class ChatServer {
         }
 
         private void broadcast(String input) {
-            synchronized (CLIENTS_LIST) { // 리스트를 락 객체로 사용
+            synchronized (CLIENTS_LIST) { // 동시에 여러 스레드가 접근할 수 있으므로 리스트를 락 객체로 사용
                 for (PrintWriter writer : CLIENTS_LIST) {
-                    writer.println(input);
+                    writer.println(input);  // 접속된 모든 클아이언트에게 메시지 전송
                 }
             }
         }
